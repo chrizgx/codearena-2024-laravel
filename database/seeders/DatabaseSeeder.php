2d9509ca2b5ade3b37bbac75ace977c065262a5a
Χρήστος Γεωργούλης <chrizgeorgoulis@gmail.com>
2024-11-20 22:31:38 +0200

    Reform database seeder to comply with model db schema changes addition of published_at

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,8 @@ use App\Models\Post;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +16,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // Create default user for login
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'info@netstudio.gr',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
+            'remember_token' => Str::random(10),
+        ]);
+
         User::factory(10)->create()->each(function (User $user) {
             $user->posts()->saveMany(Post::factory(rand(5, 10))->make());
         });
