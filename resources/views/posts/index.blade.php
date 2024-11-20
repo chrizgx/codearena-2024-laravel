dd5eae47da2cdcf3fcdae21543490dbf8d26ed8d
Χρήστος Γεωργούλης <chrizgeorgoulis@gmail.com>
2024-11-20 22:07:19 +0200

    Posts view now shows 'No posts found' when empty

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,14 +6,18 @@
       <div class="mx-auto max-w-2xl text-center">
         <h2 class="text-balance text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">From the blog</h2>
       </div>
-      <div class="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-        @foreach ($posts as $post)
-            <x-post :post="$post" />
-        @endforeach
-      </div>
-      <div class="mt-10">
-        {{ $posts->links() }}
-      </div>
+      @if(count($posts) > 0)
+        <div class="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+          @foreach ($posts as $post)
+              <x-post :post="$post" />
+          @endforeach
+        </div>
+        <div class="mt-10">
+          {{ $posts->links() }}
+        </div>
+      @else
+        <p class="mt-8 mx-auto text-center text-gray-500">No posts found.</p>
+      @endif
     </div>
   </div>
 
