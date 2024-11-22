a564727911c7c47531a1cf48ae034e9018f6b863
Χρήστος Γεωργούλης <chrizgeorgoulis@gmail.com>
2024-11-22 17:26:44 +0200

    Prepare migration and model for comment replies

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -6,10 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    protected $fillable = ['name', 'body', 'post_id', 'created_at'];
+    protected $fillable = ['name', 'body', 'post_id', 'created_at', 'parent_id', 'reference_id'];
 
     public function post()
     {
         return $this->belongsTo(Post::class);
+    }
+
+    public function replies()
+    {
+        return $this->hasMany(Comment::class, 'parent_id');
+    }
+
+    public function parent()
+    {
+        return $this->belongsTo(Comment::class, 'parent_id');
+    }
+
+    public function reference()
+    {
+        return $this->belongsTo(Comment::class, 'reference_id');
     }
 }
